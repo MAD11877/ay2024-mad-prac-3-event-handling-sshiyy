5883da19235b41e32f0675b3b6f2cc976a3c2abc
sshiyy <s10257952@connect.np.edu.sg>
2024-05-01 02:04:05 +0800

    initial commit

---
+++ app/src/main/java/sg/edu/np/mad/madpractical3/User.java
@@ -0,0 +1,22 @@
+package sg.edu.np.mad.madpractical3;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class User {
+    public String name;
+    public String description;
+    public int id;
+    public boolean followed;
+    public User(String name, String description, int id, boolean followed) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.followed = followed;
+    }
+}
