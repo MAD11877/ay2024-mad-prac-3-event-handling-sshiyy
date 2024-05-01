9b40df08f3a5ed78c504d8bad324fcc701f64239
sshiyy <s10257952@connect.np.edu.sg>
2024-05-01 20:33:22 +0800

    initial project setup

---
+++ app/src/main/java/sg/edu/np/mad/madpractical3/MessageGroup.java
@@ -0,0 +1,52 @@
+package sg.edu.np.mad.madpractical3;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import sg.edu.np.mad.madpractical3.databinding.ActivityMessageGroupBinding;
+
+public class MessageGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_message_group);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        FrameLayout frameLayout = findViewById(R.id.frameLayout);
+
+        button1.setOnClickListener(view -> {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayout, new FragmentGroup1())
+                    .commit();
+        });
+        button2.setOnClickListener(view -> {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayout, new FragmentGroup2())
+                    .commit();
+        });
+    }
+}
